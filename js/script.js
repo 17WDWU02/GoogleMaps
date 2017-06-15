3d3f72853fa4f2ed205619bf6eb738e34f22ef8f
Richard Hpa <richard.hpa@hotmail.com>
2017-06-16 11:35:50 +1200

    Added our own google maps and styled it how we want to

---
+++ js/script.js
@@ -0,0 +1,75 @@
+function init(){
+
+	var mapOptions = {
+		//Set where the Map starts
+		center:{
+			lat: -41.2950049,
+			lng:174.7814311
+		},
+		//states what the initial zoom for the map is. 
+		zoom: 17,
+		//Turn off all of the User Interface for the Map
+		disableDefaultUI: false,
+		//Turn off the ability to zoom with clicks
+		disableDoubleClickZoom: false,
+		//Turn off the ability to zoom with scroll wheel
+		scrollwheel: true,
+		//Turn off the ability to drag the map around
+		draggable: true,
+		draggableCursor: "pointer",
+		draggingCursor: "crosshair",
+		fullscreenControl: true,
+		backgroundColor: "grey",
+		keyboardShortcuts: false,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.TOP_CENTER
+		},
+		styles: [
+			{
+				stylers:[
+					{ hue: "#d01439" },
+					{ saturation: -20 }
+				]
+			},
+			{
+		        featureType: "road",
+		        elementType: "geometry",
+		        stylers: [
+		        	{ hue: "#3498db" },
+		        	{ lightness: 0 },
+		          	{ visibility: "none" }
+		        ]
+			},
+			{
+				featureType: "transit",
+				elementType: "labels",
+				stylers: [
+					{ hue: "#ff0066"},
+					{ saturation: +80 }
+				]
+			},
+			{
+				featureType: "water",
+				stylers: [
+					{ color: "#16a085"}
+				]
+			}
+		]
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', init);
+
